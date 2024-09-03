6258eaabcb6326831f01b484d1e986dfd90bc382
avcinazlinazan <avcinazlinazan@gmail.com>
2024-09-03 23:21:59 +0300

    last

---
+++ tf-files/dev/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "contactlist-backend-naz"
+    key = "env/dev/tf-remote-backend.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tf-s3-app-lock"
+    encrypt = true
+  }
+}
+
+
+module "contactlist" {
+  source   = "../modules"
+  git-user = "avcinazlinazan"
+  key-name = "anahtarkey"
+  hosted-zone = "yagmurana.info"
+  env = "dev"
+}
+
+output "websiteurl" {
+  value = module.contactlist.websiteurl
+}
--- tf-files/modules/provider.tf
+++ tf-files/modules/provider.tf
@@ -4,9 +4,18 @@ terraform {
       source = "hashicorp/aws"
       version = "5.65.0"
     }
- }
+    github = {
+      source = "integrations/github"
+      version = "6.2.3"
+    }
+  }
 }
 
 provider "aws" {
- # Configuration options
+  # Configuration options
+}
+
+provider "github" {
+  # Configuration options
+  token=data.aws_ssm_parameter.ornek_parametre.value
 }
